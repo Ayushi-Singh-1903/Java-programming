497cb1706392e918be1e8fb05aa251108b291a7f
Ayushi-Singh-1903 <singhayushi1903@gmail.com>
2021-11-30 16:22:23 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+
+public class Employee {
+
+	
+		public static void employeeDetails(String name,int DOJ,String address)
+		{
+		    System.out.print("Name Year of joining Address "+ name+" "+DOJ+" "+address);	
+		}
+		public static void main(String[] args) {
+			employeeDetails("Robert", 1994, "64C- WallsStreet Sam 2000 68D- WallsStreet John 1999 26B- WallsStreet");
+	}
+
+}
